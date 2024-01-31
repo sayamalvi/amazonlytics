a7c0be1259bce29365610a60cfaa7d86f8f46a46
Sayam Alvi <sayamalvi07@gmail.com>
2024-01-31 15:10:53 +0530

    Edited cronJob logic

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,6 +4,7 @@ import { connectToDB } from "@/lib/mongoose";
 import User from "@/lib/models/user.model";
 import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
+import Product from "@/lib/models/product.model";
 
 connectToDB();
 export async function GET() {
@@ -40,26 +41,34 @@ export async function GET() {
     try {
       const users = await User.find();
       for (const user of users) {
-        const { trackedProducts } = user;
+        const { trackedProducts, email } = user;
         if (trackedProducts.length === 0) continue;
         for (let i = 0; i < trackedProducts.length; i++) {
-          const product = trackedProducts[i];
-          const productId = product._id.toString();
+          const productObjId = trackedProducts[i];
+          const productId = trackedProducts[i].toString();
+          const product = await Product.findById(productId);
           const productURL = product.url;
-
           const scrapedProduct = await scrapeAmazonProduct(productURL);
           if (!scrapedProduct) retryScrape(productURL);
           else {
             console.log("Scraped product", scrapedProduct.currentPrice);
-            user.trackedProducts[i].priceHistory.push({
-              price: scrapedProduct.currentPrice,
-            });
+            await Product.updateOne(
+              { _id: productObjId },
+              {
+                $push: {
+                  priceHistory: {
+                    price: scrapedProduct.currentPrice,
+                    date: new Date(),
+                  },
+                },
+              }
+            );
           }
           const latestPrice =
             product.priceHistory[product.priceHistory.length - 1];
           const lastPrice =
             product.priceHistory[product.priceHistory.length - 2];
-         
+
           if (latestPrice.price < lastPrice.price) {
             notifyUser(user, product);
           }
