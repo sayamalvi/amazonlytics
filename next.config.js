ccc94485f9e2476dd637132d0516e1526a88d75f
Sayam Alvi <sayamalvi07@gmail.com>
2024-02-01 23:18:35 +0530

    fixing cron..

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/scrapeCron",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
     domains: ["m.media-amazon.com", "images-na.ssl-images-amazon.com"],
-  },
-  headers: {
-    "/api/scrapeCron": {
-      "Cache-Control": "no-store, max-age=0",
-    },
   },
 };
 
